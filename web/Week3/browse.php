13f94ba18d6efba212121ad57ec36cc8b6a4387e
Jose Gamero <josegamero@205-185-99-127.directcom.com>
2019-10-05 14:19:33 -0600

    update

---
+++ web/Week3/browse.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="shoppingCart.css">
+    <title>Hot Wheels Garage</title>
+</head>
+<body>
+    <h1>Hot Wheels Garage</h1>
+    <br>
+      <div class="flex-container">
+          <form>
+        <div>
+            <img src="Iron Man.jpg" alt="Iron Man" height="200px" width="200px">
+            <p>Iron Man Hotwheel's</p>
+            <p>Price: $50.00</p>
+            <input type="hidden" value="50" name="price">
+                    <input type="hidden" value="IronMan" name="product">                    
+                    <input type="submit" value="Add To Cart">
+        </div>
+    </form>
+    <form>
+        <div>
+            <img src="dragon.jpg" alt="Dragon" height="200px" width="200px">
+            <p>Ultimate Dragon Hotwheel's</p>
+            <p>Price: $60.00</p>
+            <input type="hidden" value="60" name="price">
+                    <input type="hidden" value="Dragon" name="product">                    
+                    <input type="submit" value="Add To Cart">
+        </div>
+    </form>
+    <form>
+        <div>
+            <img src="Hersheys.jpg" alt="Hersheys" height="200px" width="200px">
+            <p>Hershey Hotwheel's</p>
+            <p>Price: $350.00</p>
+            <input type="hidden" value="350" name="price">
+                    <input type="hidden" value="Hershey" name="product">                    
+                    <input type="submit" value="Add To Cart">
+        </div>  
+    </form>
+      </div>
+    </form>
+</body>
+</html>

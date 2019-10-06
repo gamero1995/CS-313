3fc6370c0f00aa9dde99f60ec2816816fb303895
Jose Gamero <josegamero@205-185-99-127.directcom.com>
2019-10-05 20:58:32 -0600

    update

--- web/Week3/confirmation.php
+++ web/Week3/confirmation.php
@@ -55,7 +55,7 @@ session_start()
             else {
                 echo "<img src='$Hersheys' height='150px' width='150px' alt='" . $_SESSION['cart'][$i][0] . "'>";
                 echo "<tr>";
-                echo "<td><img src='$Dragon' 'height='150px' width='150px' alt='" . $_SESSION['cart'][$i][0] . "'></td>";
+                echo "<td><img src='$Hersheys' 'height='150px' width='150px' alt='" . $_SESSION['cart'][$i][0] . "'></td>";
                 echo "<td><p>". $_SESSION['cart'][$i][0] . "</p></td>";
                 echo "<td><p>$". $_SESSION['cart'][$i][1] . "</p></td>";
                 echo "<tr>";

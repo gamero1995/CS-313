2b77cef962ebfb6332dd099bdfddb2da1e798c42
Jose Gamero <josegamero@205-185-99-127.directcom.com>
2019-10-05 21:27:33 -0600

    Done Product

--- web/Week3/confirmation.php
+++ web/Week3/confirmation.php
@@ -8,7 +8,7 @@ session_start()
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" type="text/css" href="shoppingCart.css">
-    <title>confirmation</title>
+    <title>Confirmation</title>
 </head>
 <body>
     <h1 id="titleCart">Order Confirmation</h1>
--- web/Week3/viewCart.php
+++ web/Week3/viewCart.php
@@ -8,7 +8,7 @@ session_start()
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" type="text/css" href="shoppingCart.css">
-    <title>Hot Wheels Garage</title>
+    <title>Cart</title>
 </head>
 <body>
     <h1 id="titleCart">Cart</h1>

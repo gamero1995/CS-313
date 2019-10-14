411f58c9d652fa44094041bd1f83bf4beb6d88fe
Jose Gamero <josegamero@205-185-99-127.directcom.com>
2019-10-14 17:03:19 -0600

    cleaned it up

--- db/myDb.sql
+++ db/myDb.sql
@@ -2,24 +2,23 @@ DROP TABLE appointments;
 DROP TABLE member;
 DROP TABLE services;
 
-CREATE TABLE member (
-    member_id  SERIAL PRIMARY KEY,
+CREATE TABLE member 
+(   member_id  SERIAL PRIMARY KEY,
     f_name   VARCHAR(50) NOT NULL,
     l_name   VARCHAR(50) NOT NULL,
     email    VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE services (
-    service_id SERIAL PRIMARY KEY,
+CREATE TABLE services 
+(   service_id SERIAL PRIMARY KEY,
     sevice_name VARCHAR(50) NOT NULL,
     service_description VARCHAR(100) NOT NULL,
-    service_price INT NOT NULL,
+    service_price INT NOT NULL
 );
 
-CREATE TABLE appointments (
-    appointment_id SERIAL PRIMARY KEY,
+CREATE TABLE appointments 
+(   appointment_id SERIAL PRIMARY KEY,
     appt_time    TIMESTAMP NOT NULL,
     member_id    INT NOT NULL REFERENCES member(member_id),
-    service_id    INT NOT NULL REFERENCES services (service_id),
-
+    service_id    INT NOT NULL REFERENCES services (service_id)
 );

b3ca7f10593693316f77515ac364dc88f6cd61d3
Jose Gamero <josegamero@205-185-99-127.directcom.com>
2019-10-19 21:33:21 -0600

    update

--- web/PHP web/index.php
+++ web/PHP web/index.php
@@ -5,17 +5,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="style.css" type="text/css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
     <title>Document</title>
 </head>
-<body>
-    <div class="navbar">
+<body style="height:1500px">
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+                <div class="container-fluid">
+                  <div class="navbar-header">
+                    <a class="navbar-brand" href="index.html">YourStyle</a>
+                  </div>
+                  <ul class="nav navbar-nav">
+                    <li class="active"><a href="index.html">Home</a></li>
+                    <li><a href="aboutUs.html">About Us</a></li>
+                    <li><a href="services.html">Services</a></li>
+                    <li><a href="contactUs.html">Contact us</a></li>
+                    <li><a href="logIn.html">Login</a></li>
+                  </ul>
+                </div>
+              </nav>
+    <!-- <div class="navbar">
             <ul>
                     <li><a href="aboutUs.html">About Us</a></li>
                     <li><a href="services.html">Services</a></li>
                     <li><a href="contactUs.html">Contact us</a></li>
                     <li><a href="logIn.html">Login</a></li>
                   </ul>
-    </div>
+    </div> -->
+    <br>
+    <br>
     <div class="slide-container">
         <span id="slider-image-1"></span>
         <span id="slider-image-2"></span>
@@ -32,8 +51,14 @@
             <a href="#slider-image-2" class="slider-button"></a>
             <a href="#slider-image-3" class="slider-button"></a>
     </div>
+    <br>
+    <br>
+    <div>
+        <h1>Services</h1>
+        <div class="flex-container"></div>
+        
+    </div>
     
-    <p>hello</p>
 
 </body>
 </html>

f7e0da79c3def565cc201ce32e7c90470df8595b
Jose Gamero <josegamero@205-185-99-127.directcom.com>
2019-10-19 20:56:12 -0600

    Updates

--- db/myDb.sql
+++ db/myDb.sql
@@ -1,6 +1,7 @@
 DROP TABLE appointments;
 DROP TABLE member;
 DROP TABLE services;
+DROP TABLE type_service;
 
 CREATE TABLE member 
 (   member_id  SERIAL PRIMARY KEY,
@@ -9,16 +10,38 @@ CREATE TABLE member
     email    VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE services 
-(   service_id SERIAL PRIMARY KEY,
-    sevice_name VARCHAR(50) NOT NULL,
-    service_description VARCHAR(100) NOT NULL,
-    service_price INT NOT NULL
+CREATE TABLE type_service
+(   type_service_id SERIAL PRIMARY KEY,
+    type_service_title VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE services
+(   services_id SERIAL PRIMARY KEY,
+    sevices_name VARCHAR(50) NOT NULL,
+    services_description VARCHAR(100) NOT NULL,
+    services_price INT NOT NULL,
+    type_service_id INT NOT NULL REFERENCES type_service(type_service_id)
 );
 
 CREATE TABLE appointments 
 (   appointment_id SERIAL PRIMARY KEY,
     appt_time    TIMESTAMP NOT NULL,
     member_id    INT NOT NULL REFERENCES member(member_id),
-    service_id    INT NOT NULL REFERENCES services (service_id)
+    services_id    INT NOT NULL REFERENCES services (services_id)
 );
+
+
+INSERT INTO type_service (type_service_id, type_service_title) VALUES (1, 'Hair Cut');
+INSERT INTO type_service (type_service_id, type_service_title) VALUES (2, 'Dye');
+INSERT INTO type_service (type_service_id, type_service_title) VALUES (3, 'Manicure');
+INSERT INTO type_service (type_service_id, type_service_title) VALUES (4, 'Pedicure');
+INSERT INTO type_service (type_service_id, type_service_title) VALUES (5, 'Make Up');
+INSERT INTO type_service (type_service_id, type_service_title) VALUES (6, 'Facial');
+
+INSERT INTO services (sevices_name, services_description, services_price, type_service_id) VALUES ('Short Hair', 'Basic Short Hair Haircut', 20, 1);
+INSERT INTO services (sevices_name, services_description, services_price, type_service_id) VALUES ('Basic Dye', 'Any Color Hair Dye', 20, 2);
+INSERT INTO services (sevices_name, services_description, services_price, type_service_id) VALUES ('Basic Manicure', 'One color with one design', 10, 3);
+INSERT INTO services (sevices_name, services_description, services_price, type_service_id) VALUES ('Basic Pedicure', 'One color with cleaing of toe nails', 15, 4);
+INSERT INTO services (sevices_name, services_description, services_price, type_service_id) VALUES ('Basic Make Up', 'Eye shadows and mascara', 25, 5);
+INSERT INTO services (sevices_name, services_description, services_price, type_service_id) VALUES ('Basic Facial', 'Pore cleaning with pickles on your eyes', 25,6);
+

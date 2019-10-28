326439a1d36f8c77d4b496b2f8f121cef438f5e0
Jose Gamero <josegamero@205-185-99-127.directcom.com>
2019-10-28 16:20:08 -0600

    update

--- web/PHP web/admin_page.php
+++ web/PHP web/admin_page.php
@@ -30,6 +30,10 @@ try
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="style.css" type="text/css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
     <title>Document</title>
 </head>
 <body>
@@ -39,11 +43,11 @@ try
                     <a class="navbar-brand" href="index.php">YourStyle</a>
                   </div>
                   <ul class="nav navbar-nav">
-                    <li class="active"><a href="index.php">Home</a></li>
+                    <li><a href="index.php">Home</a></li>
                     <li><a href="aboutUs.php">About Us</a></li>
                     <li><a href="services.php">Services</a></li>
                     <li><a href="contactUs.php">Contact us</a></li>
-                    <li><a href="logIn.php">Login</a></li>
+                    <li class="active"><a href="logIn.php">Login</a></li>
                   </ul>
                 </div>
               </nav>

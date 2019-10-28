68306391b52702aa2cbd5c3a96d9939ca8db0b4e
Jose Gamero <josegamero@205-185-99-127.directcom.com>
2019-10-28 16:54:27 -0600

    update

--- web/PHP web/insert_type.php
+++ web/PHP web/insert_type.php
@@ -1,7 +1,27 @@
 <?php
-
 $type = $_POST['type'];
-
+try
+    {
+      $dbUrl = getenv('DATABASE_URL');
+    
+      $dbOpts = parse_url($dbUrl);
+    
+      $dbHost = $dbOpts["host"];
+      $dbPort = $dbOpts["port"];
+      $dbUser = $dbOpts["user"];
+      $dbPassword = $dbOpts["pass"];
+      $dbName = ltrim($dbOpts["path"],'/');
+    
+      $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch (PDOException $ex)
+    {
+      echo 'Error!: ' . $ex->getMessage();
+      die();
+    }
+    
 try
 {
    $query = 'INSERT INTO type_service(type_service_title) VALUES(:type)';
